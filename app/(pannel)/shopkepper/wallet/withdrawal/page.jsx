ee6958f25f8d325afbdc1366cf7ad5959a44adea
saini8686 <ks3399094@gmail.com>
2025-04-14 09:39:45 +0530

    prod

--- app/(pannel)/shopkepper/wallet/withdrawal/page.jsx
+++ app/(pannel)/shopkepper/wallet/withdrawal/page.jsx
@@ -1,16 +1,18 @@
 import HeaderCustomer from "@/components/customer/HeaderCustomer";
-import RecentTransition from "@/components/shopkepper/wallet/RecentTransition";
-import TotalAmount from "@/components/shopkepper/wallet/TotalAmount";
 import Withdrawal from "@/components/shopkepper/wallet/withdrawal/Withdrawal";
+import { Suspense } from "react";
 
 const page = () => {
   return (
-    <div className="bg-white-low">
+   
+        <Suspense>
+          <div className="bg-white-low">
       <HeaderCustomer name="Withdrawal" />
       <div className="pb-20 mt-10 px-4">
         <Withdrawal />
       </div>
     </div>
+          </Suspense>
   );
 };
 
