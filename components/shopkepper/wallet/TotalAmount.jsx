46f5d6f59bc888003120dee45f6747638dde02de
AMIT VERMA <amit.verma@softobotics.com>
2025-07-29 19:32:18 +0530

    dsag

--- components/shopkepper/wallet/TotalAmount.jsx
+++ components/shopkepper/wallet/TotalAmount.jsx
@@ -6,23 +6,38 @@ import { useState } from "react";
 const TotalAmount = (params) => {
   const userDetails = params.total;
   const [showBalance, setShowBalance] = useState(false);
+  const [showBalance1, setShowBalance1] = useState(false);
   const [showBalance2, setShowBalance2] = useState(false);
 
   return (
     <>
-      {userDetails?.isAdmin ?
+      <div className="px-6 w-full bg-greens-900/10 rounded-lg py-4">
+        <div className="flex justify-between items-center">
+          <h2 className="text-blacks-200 text-sm !leading-130">
+            YOUR WALLET BALANCE
+          </h2>
+          <button onClick={() => setShowBalance((prev) => !prev)}>
+            <Icon icon="greenEye" />
+          </button>
+        </div>
+        <p className="text-greens-900 text-2xl !leading-130 mt-3">
+          {showBalance ? userDetails?.wallet || 0.0 : "*******"}
+        </p>
+        {/* <CustomButton url="/shopkepper/wallet/withdrawal" customClass="mt-4 w-fit">Withdraw</CustomButton> */}
+      </div>
+      {userDetails?.isAdmin &&
         <div className="flex justify-between items-center">
           <div className="px-6 w-full bg-greens-900/10 rounded-lg py-4 mr-2">
             <div className="flex justify-between items-center">
               <h2 className="text-blacks-200 text-sm !leading-130">
                 COMPANY 1
               </h2>
-              <button onClick={() => setShowBalance((prev) => !prev)}>
+              <button onClick={() => setShowBalance1((prev) => !prev)}>
                 <Icon icon="greenEye" />
               </button>
             </div>
             <p className="text-greens-900 text-2xl !leading-130 mt-3">
-              {showBalance ? userDetails?.wallet || 0.0 : "*******"}
+              {showBalance1 ? userDetails?.wallet1 || 0.0 : "*******"}
             </p>
           </div>
           <div className="px-6 w-full bg-greens-900/10 rounded-lg py-4 ms-2">
@@ -35,24 +50,9 @@ const TotalAmount = (params) => {
               </button>
             </div>
             <p className="text-greens-900 text-2xl !leading-130 mt-3">
-              {showBalance2 ? userDetails?.wallet || 0.0 : "*******"}
+              {showBalance2 ? userDetails?.wallet2 || 0.0 : "*******"}
             </p>
           </div>
-        </div>
-        :
-        <div className="px-6 w-full bg-greens-900/10 rounded-lg py-4">
-          <div className="flex justify-between items-center">
-            <h2 className="text-blacks-200 text-sm !leading-130">
-              YOUR WALLET BALANCE
-            </h2>
-            <button onClick={() => setShowBalance((prev) => !prev)}>
-              <Icon icon="greenEye" />
-            </button>
-          </div>
-          <p className="text-greens-900 text-2xl !leading-130 mt-3">
-            {showBalance ? userDetails?.wallet || 0.0 : "*******"}
-          </p>
-          {/* <CustomButton url="/shopkepper/wallet/withdrawal" customClass="mt-4 w-fit">Withdraw</CustomButton> */}
         </div>
       }
     </>
